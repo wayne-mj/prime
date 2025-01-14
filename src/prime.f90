c6ccce8055d153804287d6587d90cf6f97aadd84
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-01-14 14:58:16 +1000

    Migrated to use fpm

--- src/prime.f90
+++ src/prime.f90
@@ -1,48 +1,30 @@
-program prime
+module prime
     use iso_fortran_env
-    ! Find x prime numbers
-    use prime_module
     implicit none
+    private
 
-    integer(int32) :: primes,  number, limit
-    integer :: ierr
-    logical :: is_prime
-    character(len=100) :: arg
+    public :: prime_number
+    contains
+    ! subroutine say_hello
+    !     print *, "Hello, prime!"
+    ! end subroutine say_hello
 
-    ! Define the number of prime numbers to find
-    limit = 100**2
-    primes = 0
-    number = 0
+    logical function prime_number (num)
+        integer(int64), intent(in) :: num
+        integer(int64) :: div
 
-    print *, "Prime number finder."
-
-    call get_command_argument(1, arg, STATUS=ierr)
-
-    if (ierr .ne. 0) then
-        print *, "Usage: prime <number>"
-        stop
-    else
-        read(arg, '(I10)', iostat=ierr) limit
-        if (ierr .ne. 0) then
-            print *, "Invalid integer"
-            stop
+        if (num .le. 1_int64 ) then
+            prime_number = .false.
+            return
         end if
-    end if
 
-    if (limit .le. 0) then
-        print *, "Must not be negative"
-        stop
-    end if
+        do div = 2, int(real(num**0.5))
+            if (mod(num,div) .eq. 0) then
+                prime_number = .false.
+                return
+            end if
+        end do
 
-    ! Find the prime numbers
-    do while (primes .lt. limit)
-        is_prime = prime_number(number)
-
-        if (is_prime .eqv. .true.) then
-            primes = primes + 1
-            print *, primes, number
-        end if
-        number = number + 1
-    end do
-end program prime
-
+        prime_number = .true.
+    end function prime_number 
+end module prime
